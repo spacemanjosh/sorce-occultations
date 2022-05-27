638d0cf93635aba9bea779b915377880ff05599f
Joshua Elliott <Joshua.Elliott@lasp.colorado.edu>
2022-05-27 09:34:39 -0600

    Final changes from Claudia for last year's REU project (2021)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # import libraries
-import netCDF4 as nc
-import os
-import numpy as np
 import matplotlib.pyplot as plt
-import astropy
+import netCDF4 as nc
+import numpy as np
 from astropy.time import Time
 from matplotlib.dates import DateFormatter
-import pandas as pd
+from scipy import interpolate
+
+
+# insert link here
+def smooth(y, box_pts):
+    box = np.ones(box_pts) / box_pts
+    y_smooth = np.convolve(y, box, 'same')
+    return y_smooth
 
 
 def plot_irradianceSOL(file_name):
@@ -89,16 +94,16 @@ def plot_irradianceSOL(file_name):
             timelist.append(timebin)
             tanheightlist.append(tanheightbin)
 
-            plt.figure()
+            plt.rcParams["figure.figsize"] = [11,5]
 
             plt.plot(waveboundary, irrboundary, color='red', linestyle='dashed', label='irradiance over wavelength')
             plt.plot(wavelengthbin, irradiancebin, color='green', label='average irradiance over wavelength')
-            plt.legend(loc=9)
+            plt.legend(loc=1)
             plt.yscale("log")
             plt.xlabel("Wavelength (nm)")
             plt.ylabel("Irradiance")
             plt.savefig(
-                "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_practice\\venv\\plots--solstice_occultation20040706T181400\\"
+                "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\plots--solstice_occultation20040706T181400\\"
                 "individual_scans\\Scan" + str(count) + ".png")
             plt.close()
             count += 1
@@ -112,14 +117,38 @@ def plot_irradianceSOL(file_name):
 
         # loop through list of irradiance and time arrays to plot each element
         for i in np.arange(wavelengthlist[0, :].size):
-            plt.plot(timelist[:, i], irradiancelist[:, i])
+            # convert to julian dates
+            x = np.where(~np.isnan(timelist[:, i]))
+
+            time = Time(timelist[:, i][x], format='gps')
+            time.format = 'jd'
+            formatter = DateFormatter("%d/%m/%y %H: %M: %S:")
+
+            fig, ax = plt.subplots()
+
+            plt.plot_date(time.plot_date, irradiancelist[:, i][x])
+            ax.xaxis.set_major_formatter(formatter)
+            ax.xaxis.set_tick_params(rotation=10, labelsize=5)
+
             plt.title('Wavelength ' + "{:.2f}".format(wavelengthlist[0, i]))
             plt.xlabel("Time (GPSseconds)")
             plt.ylabel("Irradiance")
             plt.savefig(
-                "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_practice\\venv\plots--solstice_occultation20040706T181400\\"
-                "time_series_plot\\wavelength-" + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
+                "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\plots--solstice_occultation20040706T181400\\"
+                "time_series_plots\\wavelength-" + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
             plt.close()
+
+        # read O2 cross section file and assign names to variables
+        # data to be used for interpolation
+        cross_section = 'C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\O2_cross_section\\o2_cross_section.ncdf'
+        file_handle = nc.Dataset(cross_section, mode='r')
+        sigma = np.squeeze(file_handle.variables['sigma'])
+        wave = np.squeeze(file_handle.variables['wave'])
+
+        # interpolate sigma for wavelengths 120.5-121.9
+        f = interpolate.interp1d(wave, sigma)
+        sigmanew = f(wavelengthlist[0, :])
+        plt.plot(wavelengthlist[0, :], sigmanew, '-')
 
         # loop through list of irradiance and tangent height arrays to plot each element
         for i in np.arange(wavelengthlist[0, :].size):
@@ -128,15 +157,13 @@ def plot_irradianceSOL(file_name):
             plt.xlabel("Tangent Height (km)")
             plt.ylabel("Irradiance")
             plt.savefig(
-                "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_practice\\venv\\heightplot\\wavelength-" + "{:.2f}".format(
-                    wavelengthlist[0, i]) + ".png")
+                "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\plots--solstice_occultation20040706T181400\\"
+                "tanheight_plots\\wavelength-" + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
             plt.close()
 
             x = np.where((tanheightlist[:, i] >= 200) & (250 >= tanheightlist[:, i]))
             if x[0].size > 0:
                 ave_irradiance = np.mean(irradiancelist[:, i][x])
-                ########print(ave_irradiance)
-                #########print(wavelengthlist[0, i])
 
                 extinction_ratio = irradiancelist[:, i] / ave_irradiance
 
@@ -145,17 +172,72 @@ def plot_irradianceSOL(file_name):
                 plt.xlabel("Tangent Height (km)")
                 plt.ylabel("Extinction Ratio")
                 plt.savefig(
-                    "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_practice\\venv\\plots--solstice_occultation20040706T181400\\"
-                    "extinction_ratio_plot\\wavelength " + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
+                    "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\plots--solstice_occultation20040706T181400\\"
+                    "extinction_ratio_plots\\wavelength " + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
                 plt.close()
 
-        # convert to julian dates
-        time = Time(time, format='gps')
-        time.format = 'jd'
+                # column density equation, derived from extinction ratio equation
+                column_density = -np.log(extinction_ratio) / sigmanew[i]
+
+                # plot column density against tangent height in log scale
+                plt.plot(tanheightlist[:, i], column_density)
+                plt.title("Column Density at Wavelength-" + "{:.2f}".format(wavelengthlist[0, i]))
+                plt.yscale("log")
+                plt.xlabel("Tangent Height (km)")
+                plt.ylabel("Column Density (n/cm^2)")
+                plt.savefig(
+                    "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\plots--solstice_occultation20040706T181400\\"
+                    "column_density_logscale\\wavelength " + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
+                plt.close()
+
+                # plot column density against tangent height not in log scale
+                plt.plot(tanheightlist[:, i], column_density)
+                plt.title("Column Density at Wavelength-" + "{:.2f}".format(wavelengthlist[0, i]))
+                plt.xlabel("Tangent Height (km)")
+                plt.ylabel("Column Density (n/cm^2)")
+                plt.savefig(
+                    "C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\plots--solstice_occultation20040706T181400\\"
+                    "column_density\\wavelength " + "{:.2f}".format(wavelengthlist[0, i]) + ".png")
+                plt.close()
+
+
+        plt.rcParams["figure.figsize"] = [15,7]
+        alltanheightlist = []
+        column_densitylist = []
+        for i in np.arange(wavelengthlist[0, :].size):
+            x = np.where((tanheightlist[:, i] >= 200) & (250 >= tanheightlist[:, i]))
+            if x[0].size > 0:
+                ave_irradiance = np.mean(irradiancelist[:, i][x])
+                extinction_ratio = irradiancelist[:, i] / ave_irradiance
+
+                # column density equation, derived from extinction ratio equation
+                column_density = -np.log(extinction_ratio) / sigmanew[i]
+                column_densitylist.extend(column_density)
+                alltanheightlist.extend(tanheightlist[:, i])
+                plt.plot(tanheightlist[:, i], column_density, '.', label="{:.2f}".format(wavelengthlist[0, i]) + "nm")
+                plt.yscale("log")
+                plt.xlabel("Tangent Height")
+                plt.ylabel("Column Density")
+                plt.savefig("C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\scatter_plot\\plot-20040706T181400.png")
+
+        alltanheightlist = np.array(alltanheightlist, dtype=np.double)
+        x = alltanheightlist.argsort()
+        alltanheightlist = alltanheightlist[x]
+        column_densitylist = np.array(column_densitylist, dtype=np.double)
+        column_densitylist = column_densitylist[x]
+
+        x = np.where(~np.isnan(column_densitylist))
+        alltanheightlist = alltanheightlist[x]
+        column_densitylist = column_densitylist[x]
+
+
+        plt.plot(alltanheightlist, smooth(column_densitylist, 50), 'g-')
+        plt.legend(loc=0)
+        plt.savefig("C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\scatter_plot\\plot-20040706T181400.png")
 
 
 # call back my function
 if __name__ == '__main__':
-    file_name = 'C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_practice\\venv\\SOLdataset' \
-                '\\solstice_occultation20040706T181400.000.nc '
+    file_name = 'C:\\Users\\Computer\\PycharmProjects\\SOLSTICE_current_code\\venv\\SOLdatasets' \
+                '\\solstice_occultation20040706T181400.000.nc'
     plot_irradianceSOL(file_name)
