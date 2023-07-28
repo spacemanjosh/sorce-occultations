8dcf5c9e9f44161dc304777a61f478af660604d7
Joshua Elliott <Joshua.Elliott@lasp.colorado.edu>
2023-07-28 15:05:18 -0600

    Change to use "with" block to open NetCDF file
    This should close the file after each read to reduce memory usage.

--- 2023-SORCE-Gravity-Wave/occultation_package.py
+++ 2023-SORCE-Gravity-Wave/occultation_package.py
@@ -44,94 +44,94 @@ new_wave = np.array(wave.tolist())
 
 # get column density function
 def get_cd(file, plot = False):
-    ds = nc.Dataset(file)
-    irradiance = ds.variables['irradiance'][:]
-    tan_ht = ds.variables['tanht'][:]
-    wavelength = ds.variables['wavelength'][:]
-    mjd = ds.variables['julian_date'][:]
-    lat = ds.variables['lat'][:]
-    lon = ds.variables['lon'][:]
-    irr_uncertainty = ds.variables['irradiance_repeatability'][:]
-    star_name = ds.variables['star_name'][:]
-    
-    wavelength_i = np.where(wavelength > 115)
-    wavelength_1 = wavelength[wavelength_i]
-    irradiance_1 = irradiance[wavelength_i]
-    irr_uncertainty_1 = irr_uncertainty[wavelength_i]
-    tan_ht_1 = tan_ht[wavelength_i]
-    
-    dt = []
-    smoothed_irr_uncertainty= []
-    
-    for i in mjd:
-        date_time = julian.from_jd(i, fmt='jd')
-        dt.append(date_time)
-      
-    wavelength_1_ave = np.mean(wavelength_1)
-
-    f =  interpolate.interp1d(new_wave, sigma)
-    
-    new_sigma = f(wavelength_1_ave)
-
-    tanht_top = np.where((tan_ht > 240) & (tan_ht < 260))
-    places_to_ave_irr = irradiance_1[tanht_top]
-    ave_irradiance = np.mean(places_to_ave_irr)
-    ratio = irradiance_1/ave_irradiance
-    ratio[np.isnan(ratio)] = 0.0
-    w = np.where(np.isnan(ratio))
-    print(w)
-    ratio[ratio <= 0] = 0.1
-    smoothed_ratio = convolve(ratio, Box1DKernel(3))
-    smoothed_ratio[smoothed_ratio <= 0] = 0.01
-    
-    def standard_deviation_of_mean(data_set, N):
-        return np.mean(data_set) / np.sqrt(N)
-    
-    for i in range (len(irr_uncertainty_1)):
-        if i == 0:
-            uncertainty_window = [0, irr_uncertainty_1[i], irr_uncertainty_1[i+1]]
-        elif i == len(irr_uncertainty_1) - 1:
-            uncertainty_window = [irr_uncertainty_1[i-1], irr_uncertainty_1[i], 0]
-        else:
-            uncertainty_window = irr_uncertainty_1[i-1: i+2]
+    with nc.Dataset(file, 'r') as ds:
+        irradiance = ds.variables['irradiance'][:]
+        tan_ht = ds.variables['tanht'][:]
+        wavelength = ds.variables['wavelength'][:]
+        mjd = ds.variables['julian_date'][:]
+        lat = ds.variables['lat'][:]
+        lon = ds.variables['lon'][:]
+        irr_uncertainty = ds.variables['irradiance_repeatability'][:]
+        star_name = ds.variables['star_name'][:]
         
-        smoothed_irr_uncertainty.append(standard_deviation_of_mean(uncertainty_window, 3))
+        wavelength_i = np.where(wavelength > 115)
+        wavelength_1 = wavelength[wavelength_i]
+        irradiance_1 = irradiance[wavelength_i]
+        irr_uncertainty_1 = irr_uncertainty[wavelength_i]
+        tan_ht_1 = tan_ht[wavelength_i]
+        
+        dt = []
+        smoothed_irr_uncertainty= []
+        
+        for i in mjd:
+            date_time = julian.from_jd(i, fmt='jd')
+            dt.append(date_time)
+        
+        wavelength_1_ave = np.mean(wavelength_1)
+
+        f =  interpolate.interp1d(new_wave, sigma)
+        
+        new_sigma = f(wavelength_1_ave)
+
+        tanht_top = np.where((tan_ht > 240) & (tan_ht < 260))
+        places_to_ave_irr = irradiance_1[tanht_top]
+        ave_irradiance = np.mean(places_to_ave_irr)
+        ratio = irradiance_1/ave_irradiance
+        ratio[np.isnan(ratio)] = 0.0
+        w = np.where(np.isnan(ratio))
+        print(w)
+        ratio[ratio <= 0] = 0.1
+        smoothed_ratio = convolve(ratio, Box1DKernel(3))
+        smoothed_ratio[smoothed_ratio <= 0] = 0.01
+        
+        def standard_deviation_of_mean(data_set, N):
+            return np.mean(data_set) / np.sqrt(N)
+        
+        for i in range (len(irr_uncertainty_1)):
+            if i == 0:
+                uncertainty_window = [0, irr_uncertainty_1[i], irr_uncertainty_1[i+1]]
+            elif i == len(irr_uncertainty_1) - 1:
+                uncertainty_window = [irr_uncertainty_1[i-1], irr_uncertainty_1[i], 0]
+            else:
+                uncertainty_window = irr_uncertainty_1[i-1: i+2]
             
-    if plot:
-        plt.figure()
-        plt.plot(ratio, tan_ht_1)
-        plt.ylim(0, 291)
-        plt.xlabel('Extinction Ratio')
-        plt.ylabel('Tangent Height (km)')
-        plt.title('Extinction Ratio')
-    
-    #column_density_list = []
-    column_density = -np.log(ratio)/new_sigma
-    smoothed_density = -np.log(smoothed_ratio)/new_sigma
-    #column_density_list.append(column_density)
-    cd_i = np.where(tan_ht_1 < 210)
-    column_density = column_density#[cd_i]
-    column_density[np.isnan(column_density)] = 0.0
-    column_density[column_density <= 0] = 0.1
-    smoothed_density = smoothed_density#[cd_i]
-    smoothed_density[np.isnan(smoothed_density)] = 0.0
-    smoothed_density[smoothed_density <= 0] = 0.1
-    tan_ht_1 = tan_ht_1#[cd_i]
-    tan_ht_1[np.isnan(tan_ht_1)] = 0.0
-    
-    cd_uncertainty = (smoothed_irr_uncertainty)/(irradiance_1 * new_sigma)
-    
-    if plot: 
-        plt.figure()
-        fig = plt.plot(column_density, tan_ht_1, label="Column Density")
-        plt.ylim(0, 250)
-        plt.xscale('log')
-        plt.xlabel('Column Density (cm-2)')
-        plt.ylabel('Tangent Height (km)')
-        plt.legend()
-        plt.title('Column Density Vs. Tangent Height')
-    #print('test')
-    #plt.savefig("C:\\Users\\ameli\\OneDrive\\Desktop" + os.path.basename(file) + ".png")
+            smoothed_irr_uncertainty.append(standard_deviation_of_mean(uncertainty_window, 3))
+                
+        if plot:
+            plt.figure()
+            plt.plot(ratio, tan_ht_1)
+            plt.ylim(0, 291)
+            plt.xlabel('Extinction Ratio')
+            plt.ylabel('Tangent Height (km)')
+            plt.title('Extinction Ratio')
+        
+        #column_density_list = []
+        column_density = -np.log(ratio)/new_sigma
+        smoothed_density = -np.log(smoothed_ratio)/new_sigma
+        #column_density_list.append(column_density)
+        cd_i = np.where(tan_ht_1 < 210)
+        column_density = column_density#[cd_i]
+        column_density[np.isnan(column_density)] = 0.0
+        column_density[column_density <= 0] = 0.1
+        smoothed_density = smoothed_density#[cd_i]
+        smoothed_density[np.isnan(smoothed_density)] = 0.0
+        smoothed_density[smoothed_density <= 0] = 0.1
+        tan_ht_1 = tan_ht_1#[cd_i]
+        tan_ht_1[np.isnan(tan_ht_1)] = 0.0
+        
+        cd_uncertainty = (smoothed_irr_uncertainty)/(irradiance_1 * new_sigma)
+        
+        if plot: 
+            plt.figure()
+            fig = plt.plot(column_density, tan_ht_1, label="Column Density")
+            plt.ylim(0, 250)
+            plt.xscale('log')
+            plt.xlabel('Column Density (cm-2)')
+            plt.ylabel('Tangent Height (km)')
+            plt.legend()
+            plt.title('Column Density Vs. Tangent Height')
+        #print('test')
+        #plt.savefig("C:\\Users\\ameli\\OneDrive\\Desktop" + os.path.basename(file) + ".png")
     return column_density, tan_ht_1, max(dt), new_sigma, ratio, smoothed_density, cd_uncertainty, max(lat)#column_density_list
 
 def smooth_cd(file):
